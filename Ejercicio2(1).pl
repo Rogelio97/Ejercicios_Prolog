d6058321582503604c2db5458531f7a05f4c5d38
Rogelio97 <34067619+Rogelio97@users.noreply.github.com>
2020-06-01 18:46:32 -0500

    Add files via upload

---
+++ Ejercicio2(1).pl
@@ -0,0 +1,16 @@
+/*Ejercicio Práctico 2 (1) Página 27*/
+
+animal(mammal,tiger,carnivore,stripes).
+animal(mammal,hyena,carnivore,ugly).
+animal(mammal,lion,carnivore,mane).
+animal(mammal,zebra,herbivore,stripes).
+animal(bird,eagle,carnivore,large).
+animal(bird,sparrow,scavenger,small).
+animal(reptile,snake,carnivore,long).
+animal(reptile,lizard,scavenger,small).
+
+/*a)* animal(mammal,X,_,_)*/
+/*b)* animal(mammal,X,carnivore,_)*/  
+/*c)* animal(mammal,X,_,stripes)*/
+/*d) animal(reptiles,X,_,mane)*/
+
